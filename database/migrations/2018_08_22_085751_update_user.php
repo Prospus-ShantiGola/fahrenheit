4a7373e92add5f939540a96c68a929bc9613336c
Prospus-ShantiGola <shanti.gola@prospus.com>
2018-08-22 14:36:22 +0530

    adding new table field

--- database/migrations/2018_08_22_085751_update_user.php
+++ database/migrations/2018_08_22_085751_update_user.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class UpdateUser extends Migration
 {
     /**
      * Run the migrations.
@@ -14,15 +14,10 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
             $table->string('company');
             $table->string('phoneno');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
         });
+        //
     }
 
     /**
@@ -32,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        //
     }
 }
