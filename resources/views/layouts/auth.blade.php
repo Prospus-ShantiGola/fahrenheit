3629df2cac568e3e5854e9ff0eaebd254d7d87d4
Prospus-ArtiSharma <arti.sharma@prospus.com>
2018-10-17 18:37:02 +0530

    data

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="{{ asset('images/cropped-flake-32x32.png') }}" >
+    <link rel="shortcut icon" href="{{ asset('public/images/cropped-flake-32x32.png') }}" >
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -12,7 +12,7 @@
     <title>{{ config('app.name', 'Fahrenehit') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('public/js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
@@ -20,15 +20,15 @@
 
     <!-- Styles -->
 
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/css/style.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                   <img src="{{ asset('images/fahrenheit_logo.png') }}" alt="fahrenheit" />
+            <img src="{{ asset('public/images/fahrenheit_logo.png') }}" alt="fahrenheit" />
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
